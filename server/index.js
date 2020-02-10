e4234c62121d7a75ed40f3dec5182cb1b7e2c3be
mehedi <mehedi609@gmail.com>
2020-02-10 18:00:05 +0600

    vendor.js done.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const api = require('./api');
 
 const app = express();
@@ -9,20 +9,20 @@ const app = express();
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept',
-    );
-    next();
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  );
+  next();
 });
 
 app.use(cookieParser());
 
 app.use(
-    session({ secret: "banijjo", saveUninitialized: false, resave: false })
+  session({ secret: 'banijjo', saveUninitialized: false, resave: false })
 );
 
 app.use(express.urlencoded({ extended: false }));
@@ -31,5 +31,5 @@ app.use(express.json());
 app.use('/api', api);
 
 app.listen(3001, () =>
-    console.log('Express server is running on localhost:3001'),
+  console.log('Express server is running on localhost:3001')
 );
